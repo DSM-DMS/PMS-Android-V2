918c38c059c5efe40b28c58cc217385b8a1f0b10
jaewonkim <turitus@naver.com>
2021-04-16 23:21:04 +0900

    [MODIFY] features

--- app/src/main/java/com/dms/pmsandroid/ui/MainActivity.kt
+++ app/src/main/java/com/dms/pmsandroid/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dms.pmsandroid
+package com.dms.pmsandroid.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dms.pmsandroid.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
