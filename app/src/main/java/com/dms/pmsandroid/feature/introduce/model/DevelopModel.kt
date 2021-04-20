f82f7dbde403c26fbf6d332ecdd0ebaa8ecb8df0
이은별 <lbj4298@naver.com>
2021-04-20 19:43:55 +0900

    [ADD] Introduce developer

---
+++ app/src/main/java/com/dms/pmsandroid/feature/introduce/model/DevelopModel.kt
@@ -0,0 +1,6 @@
+package com.dms.pmsandroid.feature.introduce.model
+
+data class DevelopModel(
+    val developName : String,
+    val developPart : String
+)
---
+++ app/src/main/java/com/dms/pmsandroid/feature/introduce/viewmodel/IntroduceDeveloperViewModel.kt
@@ -0,0 +1,13 @@
+package com.dms.pmsandroid.feature.introduce.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+class IntroduceDeveloperViewModel : ViewModel() {
+
+    private val _developList = ListLiveData<developModel>()
+
+    val develop : LiveData<Array<developModel>>
+    get() = _developList
+
+}
