7b337d5baa81fc6c173ec78433772c5c2cd5f42a
dlswer23 <lbj4298@naver.com>
2021-05-06 19:03:37 +0900

    [MODIFY] Data List

--- app/src/main/java/com/dms/pmsandroid/feature/introduce/model/ClubsModel.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/introduce/model/ClubsModel.kt
@@ -1,3 +1,5 @@
 package com.dms.pmsandroid.feature.introduce.model
 
 data class ClubsModel(val clubs : List<String>)
+
+data class ClubDetail(val title : String,val url : String, val explanation : String, val member : List<String> )
