3b22336b4b65eb098573cd0f41c3bf078fc90785
JaeWonKim <turitus@naver.com>
2021-05-29 23:22:52 +0900

    [UPDATE]sharedViewmodel()

--- app/src/main/java/com/dms/pmsandroid/feature/login/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/login/ui/fragment/LoginFragment.kt
@@ -1,21 +1,18 @@
 package com.dms.pmsandroid.feature.login.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import com.dms.pmsandroid.R
 import com.dms.pmsandroid.base.BaseFragment
 import com.dms.pmsandroid.databinding.FragmentLoginBinding
 import com.dms.pmsandroid.feature.login.viewmodel.LoginViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
 
-    private val vm : LoginViewModel by viewModel()
+    private val vm : LoginViewModel by sharedViewModel()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
