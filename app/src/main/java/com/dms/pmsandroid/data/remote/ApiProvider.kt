50b100c676a522c5a83e16dca0b41c8d18c164db
JaeWonKim <turitus@naver.com>
2021-05-30 17:02:54 +0900

    [UPDATE]api provider const

--- app/src/main/java/com/dms/pmsandroid/data/remote/ApiProvider.kt
+++ app/src/main/java/com/dms/pmsandroid/data/remote/ApiProvider.kt
@@ -8,18 +8,18 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object ApiProvider {
-    private val BASE_URL: String = "https://api.smooth-bear.live"
-    private val CONNETCT_TIME_OUT: Long = 15
-    private val WRITE_TIME_OUT: Long = 15
-    private val READ_TIME_OUT: Long = 15
+    private const val BASE_URL: String = "https://api.smooth-bear.live"
+    private const val CONNECT_TIME_OUT: Long = 15
+    private const val WRITE_TIME_OUT: Long = 15
+    private const val READ_TIME_OUT: Long = 15
 
-    val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+    private val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
 
     private val okHttpClient: OkHttpClient = OkHttpClient().newBuilder().apply {
         addInterceptor(httpLoggingInterceptor)
-        connectTimeout(CONNETCT_TIME_OUT, TimeUnit.SECONDS)
+        connectTimeout(CONNECT_TIME_OUT, TimeUnit.SECONDS)
         writeTimeout(WRITE_TIME_OUT, TimeUnit.SECONDS)
         readTimeout(READ_TIME_OUT, TimeUnit.SECONDS)
     }.build()
