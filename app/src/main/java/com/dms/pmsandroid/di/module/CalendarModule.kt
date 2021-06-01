da2cbdd485321d7fa5fbae1713d59926b80df50f
JaeWonKim <turitus@naver.com>
2021-06-01 09:37:33 +0900

    [MODIFY]modules package

--- app/src/main/java/com/dms/pmsandroid/di/module/CalendarModule.kt
+++ app/src/main/java/com/dms/pmsandroid/di/module/CalendarModule.kt
@@ -5,5 +5,5 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val calendarModule = module {
-    viewModel { CalendarViewModel(get()) }
+    viewModel { CalendarViewModel(get(),get()) }
 }
