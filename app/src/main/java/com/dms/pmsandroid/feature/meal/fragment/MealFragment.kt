8f63d00d252f89ffd73aa99f51fd7c00fb9a552e
JaeWonKim <turitus@naver.com>
2021-06-18 22:27:57 +0900

    [UPDATE]read current time

--- app/src/main/java/com/dms/pmsandroid/feature/meal/fragment/MealFragment.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/meal/fragment/MealFragment.kt
@@ -1,10 +1,13 @@
 package com.dms.pmsandroid.feature.meal.fragment
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import com.dms.pmsandroid.R
 import com.dms.pmsandroid.base.BaseFragment
@@ -12,13 +15,22 @@ import com.dms.pmsandroid.databinding.FragmentLoginBinding
 import com.dms.pmsandroid.databinding.FragmentMealBinding
 import com.dms.pmsandroid.feature.meal.viewmodel.MealViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.util.*
 
 class MealFragment : BaseFragment<FragmentMealBinding>(R.layout.fragment_meal) {
 
     private val vm : MealViewModel by viewModel()
 
+    @SuppressLint("SimpleDateFormat")
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        vm.getMeal("20210618")
+        binding.lifecycleOwner = this
+        binding.vm = vm
+        val currentTime = Calendar.getInstance().time
+        val dateFormat = SimpleDateFormat("yyyyMMdd", Locale.KOREA).format(currentTime)
+        vm.date.value = dateFormat
     }
 }
--- app/src/main/java/com/dms/pmsandroid/feature/meal/viewmodel/MealViewModel.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/meal/viewmodel/MealViewModel.kt
@@ -1,22 +1,32 @@
 package com.dms.pmsandroid.feature.meal.viewmodel
 
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.dms.pmsandroid.data.local.SharedPreferenceStorage
 import com.dms.pmsandroid.data.remote.meal.MealApiImpl
+import java.time.format.DateTimeFormatter
 
 class MealViewModel(
     private val mealApiImpl: MealApiImpl,
     private val sharedPreferenceStorage: SharedPreferenceStorage
 ) : ViewModel() {
 
-    fun getMeal(dateTime: String) {
+    val date = MutableLiveData<String>()
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getMeal() {
+        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
         val accessToken = sharedPreferenceStorage.getInfo("access_token")
-        mealApiImpl.getMeal(accessToken, dateTime).subscribe({ response ->
-            Log.d("결과",response.raw().toString())
-        }, {
-            Log.d("결과",it.toString())
-        })
+        date.value?.let {
+            mealApiImpl.getMeal(accessToken, it.format(formatter)).subscribe({ response ->
+                Log.d("결과",response.raw().toString())
+            }, {
+                Log.d("결과",it.toString())
+            })
+        }
     }
 
 
