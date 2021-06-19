9f9333e445578aaa2e1193cfbc489fde1b6001ba
dlswer23 <lbj4298@naver.com>
2021-06-19 20:55:44 +0900

    [REMOVE] 사용하지 않는 파일

--- app/src/main/java/com/dms/pmsandroid/feature/introduce/model/ClubsModel.kt
+++
@@ -1,5 +0,0 @@
-package com.dms.pmsandroid.feature.introduce.model
-
-data class ClubsModel(val clubs : List<String>)
-
-data class ClubDetail(val title : String,val url : String, val explanation : String, val member : List<String> )
