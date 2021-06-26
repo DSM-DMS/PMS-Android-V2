ce799d60bb426e17b04b1e2aa9c60b46ebc3eda2
JaeWonKim <turitus@naver.com>
2021-06-26 22:32:00 +0900

    [UPDATE]link meals

--- app/src/main/java/com/dms/pmsandroid/feature/meal/MealAdapter.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/meal/MealAdapter.kt
@@ -9,22 +9,30 @@ import com.dms.pmsandroid.feature.meal.viewmodel.MealViewModel
 
 class MealAdapter(private val viewModel: MealViewModel) :
     RecyclerView.Adapter<MealAdapter.MealViewHolder>() {
-    private val meals = listOf<MealResponse>(
-        MealResponse(null, null, null),
-        MealResponse(null, null, null),
-        MealResponse(null, null, null)
-    )
+    private var meals = MealResponse(null, null, null)
 
     inner class MealViewHolder(private val binding: ItemMealBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(meal: MealResponse,position: Int) {
-            when(position){
-                0->binding.mealTimeTv.text = "아침"
-                1->binding.mealTimeTv.text = "점심"
-                2->binding.mealTimeTv.text = "저녁"
+        fun bind(meal: ArrayList<String>?, position: Int) {
+            when (position) {
+                0 -> binding.mealTimeTv.text = "아침"
+                1 -> binding.mealTimeTv.text = "점심"
+                2 -> binding.mealTimeTv.text = "저녁"
             }
             binding.vm = viewModel
-            binding.meal = meal
+            if (meal != null) {
+                var mealList = ""
+                for(m in meal){
+                    mealList+=m
+                }
+                if(mealList.isBlank()){
+                    binding.meal = "급식이 없습니다"
+                }else{
+                    binding.meal = mealList
+                }
+            }else{
+                binding.meal = "급식이 없습니다"
+            }
         }
     }
 
@@ -34,8 +42,13 @@ class MealAdapter(private val viewModel: MealViewModel) :
         return MealViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: MealViewHolder, position: Int) =
-        holder.bind(meals[position],position)
+    override fun onBindViewHolder(holder: MealViewHolder, position: Int) {
+        when (position) {
+            0 -> holder.bind(meals.breakfast, 0)
+            1 -> holder.bind(meals.lunch, 1)
+            2 -> holder.bind(meals.dinner, 2)
+        }
+    }
 
     override fun getItemCount(): Int = 3
 
