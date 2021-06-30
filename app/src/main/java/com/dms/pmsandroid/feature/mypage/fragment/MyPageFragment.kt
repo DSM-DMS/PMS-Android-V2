f3c97a5d6369a4ab349ec6617ba893013df78eed
JaeWonKim <turitus@naver.com>
2021-06-30 18:17:10 +0900

    Auto stash before merge of "develop" and "origin/develop"

---
+++ app/src/main/java/com/dms/pmsandroid/feature/mypage/fragment/MyPageFragment.kt
@@ -0,0 +1,18 @@
+package com.dms.pmsandroid.feature.mypage.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dms.pmsandroid.R
+import com.dms.pmsandroid.base.BaseFragment
+import com.dms.pmsandroid.databinding.FragmentMypageBinding
+
+class MyPageFragment : BaseFragment<FragmentMypageBinding>(R.layout.fragment_mypage) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
