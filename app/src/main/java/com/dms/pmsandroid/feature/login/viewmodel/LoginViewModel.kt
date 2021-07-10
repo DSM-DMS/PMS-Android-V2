1dd892311c7da5225ae56be1d386beb3c30f46df
JaeWonKim <turitus@naver.com>
2021-07-10 16:18:01 +0900

    [UPDATE]loading animation

--- app/src/main/java/com/dms/pmsandroid/feature/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/login/viewmodel/LoginViewModel.kt
@@ -13,24 +13,28 @@ class LoginViewModel(
 ) : ViewModel() {
 
     val userEmail = MutableLiveData<String>()
-    val emailDone = MutableLiveData<Boolean>(false)
+    val emailDone = MutableLiveData(false)
 
     val userPassword = MutableLiveData<String>()
-    val passwordDone = MutableLiveData<Boolean>(false)
+    val passwordDone = MutableLiveData(false)
     // SingleLiveEvent & Event Wrapper
 
-    val needRegister = MutableLiveData<Boolean>(false)
+    val needRegister = MutableLiveData(false)
 
-    val doneInput = MutableLiveData<Boolean>(false)
+    val doneInput = MutableLiveData(false)
 
-    private val _doneLogin = MutableLiveData<Boolean>(false)
+    private val _doneLogin = MutableLiveData(false)
     val doneLogin : LiveData<Boolean> get() = _doneLogin
 
     private val _toastMessage = MutableLiveData<String>()
     val toastMessage : LiveData<String> get() = _toastMessage
 
+    private val _inProgress = MutableLiveData(false)
+    val inProgress: LiveData<Boolean> get() = _inProgress
+
     fun login(){
         if(doneInput.value!!){
+            _inProgress.value = true
             val request = LoginRequest(userEmail.value!!, userPassword.value!!)
             apiImpl.loginApi(request).subscribe({
                 when(it.code()){
@@ -44,8 +48,10 @@ class LoginViewModel(
                         _toastMessage.value = "잘못된 로그인 정보입니다"
                     }
                 }
+                _inProgress.value = false
             },{
                 _toastMessage.value = "로그인에 실패하였습니다"
+                _inProgress.value = false
             })
         }
         else{
