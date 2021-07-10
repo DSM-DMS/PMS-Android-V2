948ab5590956da6a3ede6a9d851522e43b7f1c47
JaeWonKim <turitus@naver.com>
2021-07-10 16:52:31 +0900

    [UPDATE]save events

--- app/src/main/java/com/dms/pmsandroid/feature/calendar/viewmodel/CalendarViewModel.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/calendar/viewmodel/CalendarViewModel.kt
@@ -1,12 +1,10 @@
 package com.dms.pmsandroid.feature.calendar.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.dms.pmsandroid.data.local.SharedPreferenceStorage
 import com.dms.pmsandroid.data.remote.calendar.CalendarApiImpl
-import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 
 class CalendarViewModel(
@@ -16,19 +14,15 @@ class CalendarViewModel(
 
     val accessToken = sharedPreferenceStorage.getInfo("access_token")
 
-    private val _events = MutableLiveData<Map<String,String>>()
-    val events : LiveData<Map<String,String>> get() = _events
+    private val _events = MutableLiveData<MutableMap<String,String>>(HashMap())
+    val events : LiveData<MutableMap<String,String>> get() = _events
 
     fun loadSchedules() {
         calendarApiImpl.scheduleApi(accessToken).subscribe({ response ->
             if(response.isSuccessful){
                 parseEvents(response.body()!!)
             }
-            else{
-                Log.e("date",response.raw().toString())
-            }
         }, {
-            Log.e("date",it.toString())
         })
     }
 
@@ -36,7 +30,19 @@ class CalendarViewModel(
         for(month in 1..12){
             val monthEvents = body.getAsJsonObject("$month")
             val dates = monthEvents.keySet()
-            Log.d("date",dates.toString())
+            for(date in dates){
+                var eventName = "🟢  "
+                val events = monthEvents.getAsJsonArray(date)
+                eventName += events[0]
+                if(events.size()>1){
+                    for(pos in 1 until events.size()-1){
+                        eventName+="\n🟢  ${events[pos]}"
+                    }
+                }
+
+                _events.value!![date] = eventName
+            }
+
         }
     }
 
