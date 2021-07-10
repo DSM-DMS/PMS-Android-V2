64d2954ff8bbc93f5202b56700e928341588a015
JaeWonKim <turitus@naver.com>
2021-07-10 22:14:48 +0900

    [UPDATE]load token

--- app/src/main/java/com/dms/pmsandroid/feature/calendar/viewmodel/CalendarViewModel.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/calendar/viewmodel/CalendarViewModel.kt
@@ -1,5 +1,6 @@
 package com.dms.pmsandroid.feature.calendar.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,34 +10,35 @@ import com.google.gson.JsonObject
 
 class CalendarViewModel(
     private val calendarApiImpl: CalendarApiImpl,
-    sharedPreferenceStorage: SharedPreferenceStorage
+    private val sharedPreferenceStorage: SharedPreferenceStorage
 ) : ViewModel() {
 
-    val accessToken = sharedPreferenceStorage.getInfo("access_token")
-
-    private val _events = MutableLiveData<MutableMap<String,String>>(HashMap())
-    val events : LiveData<MutableMap<String,String>> get() = _events
+    private val _events = MutableLiveData<MutableMap<String, String>>(HashMap())
+    val events: LiveData<MutableMap<String, String>> get() = _events
 
     fun loadSchedules() {
+        val accessToken = sharedPreferenceStorage.getInfo("access_token")
         calendarApiImpl.scheduleApi(accessToken).subscribe({ response ->
-            if(response.isSuccessful){
+            if (response.isSuccessful) {
                 parseEvents(response.body()!!)
             }
+            Log.e("date", response.raw().toString())
         }, {
+            Log.e("date", it.toString())
         })
     }
 
-    private fun parseEvents(body:JsonObject){
-        for(month in 1..12){
+    private fun parseEvents(body: JsonObject) {
+        for (month in 1..12) {
             val monthEvents = body.getAsJsonObject("$month")
             val dates = monthEvents.keySet()
-            for(date in dates){
+            for (date in dates) {
                 var eventName = "🟢  "
                 val events = monthEvents.getAsJsonArray(date)
                 eventName += events[0]
-                if(events.size()>1){
-                    for(pos in 1 until events.size()-1){
-                        eventName+="\n🟢  ${events[pos]}"
+                if (events.size() > 1) {
+                    for (pos in 1 until events.size() - 1) {
+                        eventName += "\n🟢  ${events[pos]}"
                     }
                 }
 
