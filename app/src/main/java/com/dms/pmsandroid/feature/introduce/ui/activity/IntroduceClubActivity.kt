a8a8fd8d307457d44361b2d9762ebb3ce2a95ee2
dlswer23 <lbj4298@naver.com>
2021-07-18 19:15:04 +0900

    [ADD] clubDetail page
    하지만 오류가 있지 ..

--- app/src/main/java/com/dms/pmsandroid/feature/introduce/ui/activity/IntroduceClubActivity.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/introduce/ui/activity/IntroduceClubActivity.kt
@@ -1,5 +1,6 @@
 package com.dms.pmsandroid.feature.introduce.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
 import com.dms.pmsandroid.R
@@ -7,6 +8,7 @@ import com.dms.pmsandroid.base.BaseActivity
 import com.dms.pmsandroid.databinding.ActivityIntroduceClubBinding
 import com.dms.pmsandroid.feature.introduce.adapter.ClubAdapter
 import com.dms.pmsandroid.feature.introduce.viewmodel.IntroduceClubViewModel
+import com.dms.pmsandroid.generated.callback.OnClickListener
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class IntroduceClubActivity :
@@ -15,15 +17,20 @@ class IntroduceClubActivity :
     override val vm: IntroduceClubViewModel by viewModel()
     private val clubAdapter by lazy { ClubAdapter(vm) }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         vm.loadClubs()
+        intent()
         val gridLayoutManager = GridLayoutManager(this, 2)
         binding.introClubRc.layoutManager = gridLayoutManager
         binding.introClubRc.adapter = clubAdapter
         binding.backImg.setOnClickListener() {
             finish()
         }
+    }
+
+    private fun checkClick() {
 
     }
 
@@ -31,5 +38,11 @@ class IntroduceClubActivity :
         vm.clubs.observe(this, {
             clubAdapter.setItem(it.clubs)
         })
+    }
+
+    fun intent() {
+
+        val intent = Intent(this, IntroduceClubDetailActivity::class.java)
+        startActivity(intent)
     }
 }
