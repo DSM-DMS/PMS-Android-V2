02ea0c74db5cda5ac3ae8eff03ca36897ab55a2d
JaeWonKim <turitus@naver.com>
2021-07-28 13:25:24 +0900

    [UPDATE]notice search subscribe

--- app/src/main/java/com/dms/pmsandroid/feature/notify/ui/fragment/InNoticeFragment.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/notify/ui/fragment/InNoticeFragment.kt
@@ -1,6 +1,7 @@
 package com.dms.pmsandroid.feature.notify.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,6 +12,8 @@ import com.dms.pmsandroid.feature.notify.adapter.NoticeAdapter
 import com.dms.pmsandroid.feature.notify.viewmodel.NotifyViewModel
 import com.dms.pmsandroid.ui.MainActivity
 import com.jakewharton.rxbinding4.widget.textChanges
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.Disposable
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import java.util.concurrent.TimeUnit
 
@@ -25,28 +28,33 @@ class InNoticeFragment : BaseFragment<FragmentInNoticeBinding>(R.layout.fragment
         super.onViewCreated(view, savedInstanceState)
         val noticeLayoutManager = LinearLayoutManager(binding.noticeRv.context)
         noticeLayoutManager.orientation = RecyclerView.VERTICAL
-        binding.noticeRv.run{
+        binding.noticeRv.run {
             adapter = noticeAdapter
             layoutManager = noticeLayoutManager
         }
+
         vm.getNoticeList(0)
     }
+
     override fun observeEvent() {
-        vm.noticeList.observe(viewLifecycleOwner,{
+        vm.noticeList.observe(viewLifecycleOwner, {
             noticeAdapter.setItems(it)
         })
-        vm.clickedNoticeId.observe(viewLifecycleOwner,{
-            (activity as MainActivity).startNoticeDetail(it,vm.clickedNoticeTitle)
+        vm.clickedNoticeId.observe(viewLifecycleOwner, {
+            (activity as MainActivity).startNoticeDetail(it, vm.clickedNoticeTitle)
         })
-        binding.noticeEt.textChanges().debounce(500,TimeUnit.MILLISECONDS).subscribe{
-            if (it.isNotEmpty()){
-                binding.noticeLl.visibility = View.GONE
-                vm.searchNotice(it.toString())
-            }else{
-                binding.noticeLl.visibility = View.VISIBLE
-                vm.resetNoticePage()
-                vm.getNoticeList(0)
+        binding.noticeEt.textChanges().debounce(500, TimeUnit.MILLISECONDS).map { it.toString() }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                if (it.isNotBlank()) {
+                    Log.d("etError", "no $it")
+                    vm.searchNotice(it.toString())
+                    binding.noticeLl.visibility = View.GONE
+                } else {
+                    vm.resetNoticePage()
+                    vm.getNoticeList(0)
+                    binding.noticeLl.visibility = View.VISIBLE
+                }
             }
-        }
     }
 }
