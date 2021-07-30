1968c38d6d52a4bd75c9f53df775462afcc6da18
JaeWonKim <turitus@naver.com>
2021-07-30 19:14:42 +0900

    [ADD]introduce club detail viewmodel di

--- app/src/main/java/com/dms/pmsandroid/di/module/IntroduceModule.kt
+++ app/src/main/java/com/dms/pmsandroid/di/module/IntroduceModule.kt
@@ -1,6 +1,7 @@
 package com.dms.pmsandroid.di.module
 
 import com.dms.pmsandroid.data.remote.Introduce.IntroduceClubApiImpl
+import com.dms.pmsandroid.feature.introduce.viewmodel.IntroduceClubDetailViewModel
 import com.dms.pmsandroid.feature.introduce.viewmodel.IntroduceClubViewModel
 import com.dms.pmsandroid.feature.introduce.viewmodel.IntroduceCompanyViewModel
 import com.dms.pmsandroid.feature.introduce.viewmodel.MainIntroduceViewModel
@@ -15,5 +16,7 @@ val introduceModule = module {
     viewModel { IntroduceClubViewModel(get(),get()) }
 
     viewModel { IntroduceCompanyViewModel(get()) }
+
+    viewModel {IntroduceClubDetailViewModel(get(),get())}
 
 }
