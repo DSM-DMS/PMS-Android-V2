be7d4901c074dd3d3aa1fdb0824c9d674a5a499c
dlswer23 <lbj4298@naver.com>
2021-08-17 19:35:29 +0900

    [UPDATE] outing

--- app/src/main/java/com/dms/pmsandroid/feature/mypage/ui/activity/OutingContentActivity.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/mypage/ui/activity/OutingContentActivity.kt
@@ -1,16 +1,32 @@
 package com.dms.pmsandroid.feature.mypage.ui.activity
 
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.dms.pmsandroid.R
 import com.dms.pmsandroid.base.BaseActivity
 import com.dms.pmsandroid.databinding.ActivityOutingContentBinding
+import com.dms.pmsandroid.feature.introduce.ui.HorizontalItemDecorator
+import com.dms.pmsandroid.feature.mypage.adapter.OutingAdapter
 import com.dms.pmsandroid.feature.mypage.viewmodel.OutingContentViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class OutingContentActivity : BaseActivity<ActivityOutingContentBinding>(R.layout.activity_outing_content) {
 
     override val vm : OutingContentViewModel by viewModel()
+    private val outingAdapter by lazy { OutingAdapter(vm) }
 
-    override fun observeEvent() {
-        TODO("Not yet implemented")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        val number =
+//        vm.loadOuting(number)
+        val gridLayoutManager = GridLayoutManager(this, 2)
+        binding.outingRc.addItemDecoration(HorizontalItemDecorator(80))
+        binding.outingRc.adapter = outingAdapter
+
     }
+    override fun observeEvent() {
+
+
+    }
+
 }
--- app/src/main/java/com/dms/pmsandroid/feature/mypage/viewmodel/OutingContentViewModel.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/mypage/viewmodel/OutingContentViewModel.kt
@@ -3,12 +3,23 @@ package com.dms.pmsandroid.feature.mypage.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.dms.pmsandroid.data.local.SharedPreferenceStorage
 import com.dms.pmsandroid.data.remote.mypage.MyPageApiImpl
+import com.dms.pmsandroid.feature.mypage.model.OutingListResponse
 import com.dms.pmsandroid.feature.mypage.model.OutingResponse
 
-class OutingContentViewModel(private val myPageApiImpl: MyPageApiImpl):ViewModel() {
+class OutingContentViewModel(private val myPageApiImpl: MyPageApiImpl,private val sharedPreferenceStorage: SharedPreferenceStorage):ViewModel() {
 
-    private val _outing = MutableLiveData<List<OutingResponse>>()
-    val outings : LiveData<List<OutingResponse>> get() = _outing
+    private val _outing = MutableLiveData<OutingListResponse>()
+    val outings : MutableLiveData<OutingListResponse> get() = _outing
 
+    fun loadOuting(number : Int){
+        val accessToken = sharedPreferenceStorage.getInfo("access_token")
+        myPageApiImpl.getOutingApi(accessToken,number).subscribe({
+            if(it.isSuccessful) {
+                _outing.value = it.body()
+            }
+        }, {
+        })
+    }
 }
