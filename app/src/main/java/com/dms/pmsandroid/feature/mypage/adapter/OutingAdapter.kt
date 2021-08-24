2fbee16b83b467665b1aee37a517dc589efba06e
dlswer23 <lbj4298@naver.com>
2021-08-24 23:40:42 +0900

    [UPDATE] outing all

--- app/src/main/java/com/dms/pmsandroid/feature/mypage/adapter/OutingAdapter.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/mypage/adapter/OutingAdapter.kt
@@ -1,26 +1,32 @@
 package com.dms.pmsandroid.feature.mypage.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.dms.pmsandroid.R
 import com.dms.pmsandroid.databinding.ItemOutingBinding
 import com.dms.pmsandroid.feature.mypage.model.OutingResponse
 import com.dms.pmsandroid.feature.mypage.viewmodel.OutingContentViewModel
 
-class OutingAdapter(private val viewModel: OutingContentViewModel) :
+class OutingAdapter(private val viewModel: OutingContentViewModel, context: Context) :
     RecyclerView.Adapter<OutingAdapter.OutingViewHolder>() {
     private var outingList = ArrayList<OutingResponse>()
 
+    private val blue = context.resources.getColor(R.color.blue)
+    private val red = context.resources.getColor(R.color.red)
 
     inner class OutingViewHolder(private val binding: ItemOutingBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(position:Int) {
-            binding.date = "날짜" + outingList[position].date.toString()
-            binding.reason =  "이유" +outingList[position].reason
-            binding.place =  "장소" +outingList[position].place
+        fun bind(position: Int) {
+            binding.date = outingList[position].date
+            binding.reason = "사유" + outingList[position].reason
+            binding.place = "장소" + outingList[position].place
+            if (outingList[position].type == "DISEASE") {
+                binding.checkV.setBackgroundColor(red)
+            } else
+                binding.checkV.setBackgroundColor(blue)
             binding.vm = viewModel
-            binding.executePendingBindings()
-            binding.notifyChange();
         }
     }
 
--- app/src/main/java/com/dms/pmsandroid/feature/mypage/ui/activity/OutingContentActivity.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/mypage/ui/activity/OutingContentActivity.kt
@@ -4,40 +4,48 @@ import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.dms.pmsandroid.R
 import com.dms.pmsandroid.base.BaseActivity
 import com.dms.pmsandroid.databinding.ActivityOutingContentBinding
 import com.dms.pmsandroid.feature.introduce.ui.HorizontalItemDecorator
+import com.dms.pmsandroid.feature.introduce.ui.VerticalItemDecorator
 import com.dms.pmsandroid.feature.mypage.adapter.OutingAdapter
 import com.dms.pmsandroid.feature.mypage.viewmodel.OutingContentViewModel
+import org.koin.android.ext.android.bind
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class OutingContentActivity : BaseActivity<ActivityOutingContentBinding>(R.layout.activity_outing_content) {
 
-    override val vm : OutingContentViewModel by viewModel()
-    private val outingAdapter by lazy { OutingAdapter(vm) }
+class OutingContentActivity :
+    BaseActivity<ActivityOutingContentBinding>(R.layout.activity_outing_content) {
+
+    override val vm: OutingContentViewModel by viewModel()
+    private val outingAdapter by lazy { OutingAdapter(vm, this) }
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val number = intent.getIntExtra("number",0)
-        if(number!=0){
+        val number = intent.getIntExtra("number", 0)
+        if (number != 0) {
             vm.loadOuting(number)
         }
         binding.outingRc.adapter = outingAdapter
-        binding.outingRc.addItemDecoration(HorizontalItemDecorator(25))
-        binding.backImg.setOnClickListener{
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = RecyclerView.VERTICAL
+        binding.outingRc.layoutManager = layoutManager
+        binding.backImg.setOnClickListener {
             finish()
         }
 
     }
+
     override fun observeEvent() {
         vm.run {
-            outings.observe(this@OutingContentActivity,{
+            outings.observe(this@OutingContentActivity, {
                 outingAdapter.setItem(it.outings)
             })
         }
-
     }
 
 }
