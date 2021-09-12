dd9ea7cf9f23a6a11865f3cc20ab950dd8b8bc91
Jaewon <turitus@naver.com>
2021-09-12 21:46:24 +0900

    [UPDATE]shimmer layout

--- app/src/main/java/com/dms/pmsandroid/feature/mypage/ui/activity/PointContentActivity.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/mypage/ui/activity/PointContentActivity.kt
@@ -2,13 +2,13 @@ package com.dms.pmsandroid.feature.mypage.ui.activity
 
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dms.pmsandroid.R
 import com.dms.pmsandroid.base.BaseActivity
 import com.dms.pmsandroid.databinding.ActivityPointContentBinding
-import com.dms.pmsandroid.feature.introduce.ui.HorizontalItemDecorator
 import com.dms.pmsandroid.feature.mypage.adapter.PointAdapter
 import com.dms.pmsandroid.feature.mypage.viewmodel.PointContentViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -17,7 +17,7 @@ class PointContentActivity :
     BaseActivity<ActivityPointContentBinding>(R.layout.activity_point_content) {
 
     override val vm: PointContentViewModel by viewModel()
-    private val pointAdapter by lazy { PointAdapter(vm,binding.pointRc.context) }
+    private val pointAdapter by lazy { PointAdapter(vm, binding.pointRc.context) }
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,16 +29,23 @@ class PointContentActivity :
         binding.pointRc.adapter = pointAdapter
         val layoutManager = LinearLayoutManager(this)
         layoutManager.orientation = RecyclerView.VERTICAL
-        binding.pointRc.layoutManager = layoutManager
-        binding.backImg.setOnClickListener {
-            finish()
+        binding.run {
+            pointRc.layoutManager = layoutManager
+            backImg.setOnClickListener {
+                finish()
+            }
+            pointShimmerFl.startShimmer()
+            binding.pointRc.visibility = View.INVISIBLE
         }
     }
 
     override fun observeEvent() {
         vm.run {
-            points.observe(this@PointContentActivity,{
+            points.observe(this@PointContentActivity, {
                 pointAdapter.setItem(it.points)
+                binding.pointShimmerFl.hideShimmer()
+                binding.pointShimmerFl.visibility = View.INVISIBLE
+                binding.pointRc.visibility = View.VISIBLE
             })
         }
     }
