3768bf4dba4d65fc1f9b47123250b0729ee5b133
Jaewon <turitus@naver.com>
2021-10-02 22:48:54 +0900

    [UPDATE] find password ui

--- app/src/main/java/com/dms/pmsandroid/feature/login/viewmodel/FindPassWordViewModel.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/login/viewmodel/FindPassWordViewModel.kt
@@ -5,6 +5,12 @@ import androidx.lifecycle.ViewModel
 
 class FindPassWordViewModel: ViewModel() {
     val email = MutableLiveData<String>()
+
+    val inProgress = MutableLiveData<Boolean>()
+
+    fun sendEmail(){
+
+    }
     //유효한 이메일 아니면 401
     //인증시간 30분
 
