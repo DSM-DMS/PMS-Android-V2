6f747b5681386082d742c920c28186cc2a0e687a
Jaewon <turitus@naver.com>
2021-10-02 21:44:58 +0900

    [CREATE] find password viewmodel

---
+++ app/src/main/java/com/dms/pmsandroid/feature/login/viewmodel/FindPassWordViewModel.kt
@@ -0,0 +1,11 @@
+package com.dms.pmsandroid.feature.login.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class FindPassWordViewModel: ViewModel() {
+    val email = MutableLiveData<String>()
+    //유효한 이메일 아니면 401
+    //인증시간 30분
+
+}
