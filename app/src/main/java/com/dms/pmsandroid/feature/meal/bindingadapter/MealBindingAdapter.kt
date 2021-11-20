c1f26452b19d3473c68d79d67e301e95f6f050a5
Jaewon <turitus@naver.com>
2021-11-20 23:31:02 +0900

    [UPDATE] meal binding adapter

--- app/src/main/java/com/dms/pmsandroid/feature/meal/bindingadapter/MealBindingAdapter.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/meal/bindingadapter/MealBindingAdapter.kt
@@ -1,46 +1,42 @@
 package com.dms.pmsandroid.feature.meal.bindingadapter
 
 import android.annotation.SuppressLint
-import android.graphics.Color
-import android.graphics.PorterDuff
-import android.widget.ImageView
+import android.os.Build
 import android.widget.TextView
-import androidx.core.content.ContextCompat
+import androidx.annotation.RequiresApi
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import androidx.viewpager2.widget.ViewPager2
-import com.bumptech.glide.Glide
-import com.dms.pmsandroid.R
+import java.time.LocalDate
 
-object MealBindingAdapter {
-    @SuppressLint("SetTextI18n")
-    @JvmStatic
-    @BindingAdapter("meal_time")
-    fun mealTime(textView: TextView,time:String){
-        textView.text = "${time.substring(0,4)}/${time.substring(4,6)}/${time.substring(6,8)}"
+@RequiresApi(Build.VERSION_CODES.O)
+@SuppressLint("SetTextI18n")
+@BindingAdapter("meal_time")
+fun mealTime(textView: TextView, time: LocalDate) {
+    textView.text = "${time.year}/${String.format("%02d", time.monthValue)}/${String.format("%02d", time.dayOfMonth)}"
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@BindingAdapter("week_time")
+fun weekTime(textView: TextView, week: LocalDate) {
+    when (week.dayOfWeek.value) {
+        1 -> textView.text = "월요일"
+        2 -> textView.text = "화요일"
+        3 -> textView.text = "수요일"
+        4 -> textView.text = "묙요일"
+        5 -> textView.text = "금요일"
+        6 -> textView.text = "토요일"
+        7 -> textView.text = "일요일"
     }
+}
 
-    @JvmStatic
-    @BindingAdapter("week_time")
-    fun weekTime(textView:TextView,week:Int){
-        when(week){
-            1->textView.text = "월요일"
-            2->textView.text = "화요일"
-            3->textView.text = "수요일"
-            4->textView.text = "묙요일"
-            5->textView.text = "금요일"
-            6->textView.text = "토요일"
-            7->textView.text = "일요일"
-        }
-    }
-
-    @JvmStatic
-    @BindingAdapter("view_pager_adapter")
-    fun recyclerViewHorAdapter(viewPager: ViewPager2, adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>?){
-        if(adapter!=null){
-            viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-            viewPager.adapter = adapter
-        }
+@BindingAdapter("view_pager_adapter")
+fun recyclerViewHorAdapter(
+    viewPager: ViewPager2,
+    adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>?
+) {
+    if (adapter != null) {
+        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        viewPager.adapter = adapter
     }
 }
