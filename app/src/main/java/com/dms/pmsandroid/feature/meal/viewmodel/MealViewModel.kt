afa307603286f0b04e98a58a6a5d1de7ec7e8641
Jaewon <turitus@naver.com>
2021-12-19 23:20:58 +0900

    [MODIFY] clean meal view model

--- app/src/main/java/com/dms/pmsandroid/feature/meal/viewmodel/MealViewModel.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/meal/viewmodel/MealViewModel.kt
@@ -1,26 +1,15 @@
 package com.dms.pmsandroid.feature.meal.viewmodel
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.dms.pmsandroid.data.local.SharedPreferenceStorage
-import com.dms.pmsandroid.data.meal.remote.ProvideMealApi
+import com.dms.pmsandroid.data.meal.repository.MealRepository
 import com.dms.pmsandroid.feature.meal.entity.MealItem
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.schedulers.Schedulers
-import java.time.LocalDate
 import kotlin.collections.HashMap
 
 class MealViewModel(
-    private val provideMealApi: ProvideMealApi,
-    private val sharedPreferenceStorage: SharedPreferenceStorage
+    private val mealRepository: MealRepository
 ) : ViewModel() {
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    val date = MutableLiveData(LocalDate.now())
 
     val currentPosition = MutableLiveData(Int.MAX_VALUE / 2)
 
@@ -30,109 +19,77 @@ class MealViewModel(
     private val _meals = MutableLiveData(HashMap<Int, MealItem>())
     val meals: LiveData<HashMap<Int, MealItem>> get() = _meals
 
-    @RequiresApi(Build.VERSION_CODES.O)
     fun clickNextDay() {
-        currentPosition.value = currentPosition.value!! + (2 - (currentPosition.value!! % 3))
+        movePositionToNextDayMorning()
         nextDay()
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     fun nextDay() {
-        currentPosition.value = currentPosition.value!! + 1
-        date.value = date.value!!.plusDays(1)
+        plusOneToPosition()
         loadNextDayMeal()
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     fun clickBeforeDay() {
-        currentPosition.value = currentPosition.value!! - (currentPosition.value!! % 3)
+        movePositionToBeforeDayMorning()
         beforeDay()
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     fun beforeDay() {
-        currentPosition.value = currentPosition.value!! - 1
-        date.value = date.value!!.minusDays(1)
+        minusOneToPosition()
         loadBeforeDayMeal()
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun loadNextDayMeal() {
-        getMeal(isBeforeDay = false)
+    private fun movePositionToNextDayMorning() {
+        currentPosition.value = currentPosition.value!! + (2 - (currentPosition.value!! % 3))
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun loadBeforeDayMeal() {
-        getMeal(isBeforeDay = true)
+    private fun movePositionToBeforeDayMorning() {
+        currentPosition.value = currentPosition.value!! - (currentPosition.value!! % 3)
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun getMeal(isBeforeDay: Boolean) {
-        val accessToken = sharedPreferenceStorage.getInfo("access_token")
-        val position = if(isBeforeDay) currentPosition.value!! - 2 else currentPosition.value!!
+    private fun plusOneToPosition() {
+        currentPosition.value = currentPosition.value!! + 1
+    }
 
-        if(!meals.value!!.containsKey(position)) {
-            provideMealApi.getMealItem(accessToken, date.value!!.toDateTimeString(), position)
-                .subscribe { meal ->
-                    _meals.value = meals.value!!.apply {
-                        putAll(meal)
-                    }
-                }
+    private fun minusOneToPosition() {
+        currentPosition.value = currentPosition.value!! - 1
+    }
+
+
+    private fun loadNextDayMeal() {
+        val position = currentPosition.value!!
+
+        if(positionIsEmpty(position)) {
+            appendMealItems(position)
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun getInitMeal() {
-        val accessToken = sharedPreferenceStorage.getInfo("access_token")
+    private fun loadBeforeDayMeal() {
+        val position = currentPosition.value!! - 2
 
-        val currentDate = date.value!!
-        val nextDate = currentDate.plusDays(1)
-        val beforeDate = currentDate.minusDays(1)
-
-        val todayMeal = provideMealApi.getMealItem(
-            accessToken,
-            currentDate.toDateTimeString(),
-            currentPosition.value!!
-        )
-        val nextDayMeal = provideMealApi.getMealItem(
-            accessToken,
-            nextDate.toDateTimeString(),
-            currentPosition.value!! + 3
-        )
-        val lastDayMeal = provideMealApi.getMealItem(
-            accessToken,
-            beforeDate.toDateTimeString(),
-            currentPosition.value!! - 3
-        )
-        Single.zip(
-            todayMeal,
-            nextDayMeal,
-            lastDayMeal,
-            { t1, t2, t3 ->
-                HashMap<Int, MealItem>().apply {
-                    putAll(t1)
-                    putAll(t2)
-                    putAll(t3)
-                }
-            }
-        )
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io())
-            .subscribe { mealList ->
-                _meals.value = mealList
-            }
+        if(positionIsEmpty(position)) {
+            appendMealItems(position)
+        }
     }
 
+    private fun appendMealItems(position: Int) {
+        mealRepository.getBeforeDayMeal(position).subscribe { result ->
+            _meals.value!!.putAll(result)
+        }
+    }
 
+    private fun positionIsEmpty(position: Int): Boolean = !meals.value!!.containsKey(position)
+
+    fun getInitMeal() {
+        mealRepository.getYesterdayTodayTomorrowMeals(currentPosition.value!!).subscribe { result ->
+            _meals.value = result
+        }
+    }
 
     fun showPicture(hasPicture: Boolean) {
         if (hasPicture) {
             _showPicture.value = !showPicture.value!!
         }
     }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun LocalDate.toDateTimeString() =
-        "${this.year}${String.format("%02d", this.monthValue)}${String.format("%02d", this.dayOfMonth)}"
 
 }
