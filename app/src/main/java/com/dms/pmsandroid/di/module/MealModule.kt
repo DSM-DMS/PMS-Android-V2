c56e9610153002b4a0b25d80819777e7dff3f1ed
Jaewon <turitus@naver.com>
2021-12-20 10:23:30 +0900

    [UPDATE] meal repository di

--- app/src/main/java/com/dms/pmsandroid/di/module/MealModule.kt
+++ app/src/main/java/com/dms/pmsandroid/di/module/MealModule.kt
@@ -1,10 +1,13 @@
 package com.dms.pmsandroid.di.module
 
 import com.dms.pmsandroid.data.meal.remote.ProvideMealApi
+import com.dms.pmsandroid.data.meal.repository.MealRepository
+import com.dms.pmsandroid.data.meal.repository.MealRepositoryImpl
 import com.dms.pmsandroid.feature.meal.viewmodel.MealViewModel
 import org.koin.dsl.module
 
 val mealModule = module {
-    single { ProvideMealApi(get()) }
-    single { MealViewModel(get(),get()) }
+    single<MealRepository> { MealRepositoryImpl(get()) }
+    single { ProvideMealApi(get(), get()) }
+    single { MealViewModel(get()) }
 }
