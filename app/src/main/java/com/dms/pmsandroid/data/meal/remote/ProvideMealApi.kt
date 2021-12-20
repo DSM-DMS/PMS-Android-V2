35ef72bd5ecba117f923dd4fb4e403581d0046ad
Jaewon <turitus@naver.com>
2021-12-20 10:22:52 +0900

    [UPDATE] provide meal api

--- app/src/main/java/com/dms/pmsandroid/data/meal/remote/ProvideMealApi.kt
+++ app/src/main/java/com/dms/pmsandroid/data/meal/remote/ProvideMealApi.kt
@@ -1,5 +1,6 @@
 package com.dms.pmsandroid.data.meal.remote
 
+import com.dms.pmsandroid.data.local.SharedPreferenceStorage
 import com.dms.pmsandroid.data.remote.PotatoChipApi
 import com.dms.pmsandroid.feature.meal.entity.MealItem
 import com.dms.pmsandroid.feature.meal.entity.toMealText
@@ -12,43 +13,58 @@ import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.functions.BiFunction
 import io.reactivex.rxjava3.schedulers.Schedulers
 import retrofit2.Response
+import java.time.LocalDate
 import java.util.*
 import kotlin.collections.HashMap
 
-class ProvideMealApi(api: PotatoChipApi) {
+class ProvideMealApi(api: PotatoChipApi, private val sharedPreferenceStorage: SharedPreferenceStorage) {
 
     private val mealApi = api.retrofit.create(MealApi::class.java)
 
-    private fun getMeal(accessToken: String, dateTime: String): @NonNull Single<Response<MealResponse>> =
+    private val accessToken by lazy {
+        sharedPreferenceStorage.getInfo("access_token")
+    }
+
+    private fun getMeal(dateTime: String): @NonNull Single<Response<MealResponse>> =
         mealApi.getMeal(accessToken, dateTime)
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
 
     private fun getMealPicture(
-        accessToken: String,
         dateTime: String
     ): @NonNull Single<Response<MealPictureResponse>> =
         mealApi.getMealPicture(accessToken, dateTime)
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
 
-    fun getMealItem(accessToken: String, dateTime: String, currentPosition: Int): @NonNull Single<HashMap<Int, MealItem>> =
-        Single.zip(getMeal(accessToken, dateTime),getMealPicture(accessToken, dateTime), BiFunction{t1, t2 ->
-            if(t1.isSuccessful){
-                if(t1.body() != null) {
+    fun getMealItem(dateTime: LocalDate, currentPosition: Int): @NonNull Single<HashMap<Int, MealItem>> =
+        Single.zip(getMeal(dateTime.toDateTimeString()),getMealPicture(dateTime.toDateTimeString()), BiFunction{response, t2 ->
+            if(response.isSuccessful && response.body() != null){
                     return@BiFunction HashMap<Int, MealItem>().apply {
-                        put(currentPosition, MealItem(t1.body()!!.toEntity().breakfast.toMealText(), t2.body()?.breakfast))
-                        put(currentPosition + 1, MealItem(t1.body()!!.toEntity().lunch.toMealText(), t2.body()?.lunch))
-                        put(currentPosition + 2, MealItem(t1.body()!!.toEntity().dinner.toMealText(),t2.body()?.dinner))
+                        put(
+                            currentPosition,
+                            MealItem(
+                                response.body()!!.toEntity().breakfast.toMealText(),
+                                t2.body()?.breakfast
+                            )
+                        )
+                        put(
+                            currentPosition + 1,
+                            MealItem(response.body()!!.toEntity().lunch.toMealText(), t2.body()?.lunch)
+                        )
+                        put(
+                            currentPosition + 2,
+                            MealItem(response.body()!!.toEntity().dinner.toMealText(), t2.body()?.dinner)
+                        )
                     }
-                } else {
-                    return@BiFunction HashMap<Int, MealItem>()
-                }
-            }else {
+            } else {
                 return@BiFunction HashMap<Int, MealItem>()
             }
         })
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
+
+    private fun LocalDate.toDateTimeString() =
+        "${this.year}${String.format("%02d", this.monthValue)}${String.format("%02d", this.dayOfMonth)}"
 
 }
