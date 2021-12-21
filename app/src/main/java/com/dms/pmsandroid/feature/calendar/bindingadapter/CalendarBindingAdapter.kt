efaa1a8b25747050ec027d089469333b29681007
Jaewon <turitus@naver.com>
2021-12-21 23:19:09 +0900

    [UPDATE] calendar fragment ui

---
+++ app/src/main/java/com/dms/pmsandroid/feature/calendar/bindingadapter/CalendarBindingAdapter.kt
@@ -0,0 +1,35 @@
+package com.dms.pmsandroid.feature.calendar.bindingadapter
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.dms.pmsandroid.R
+import com.dms.pmsandroid.feature.calendar.model.EventModel
+import com.dms.pmsandroid.feature.calendar.toEmoji
+import com.prolificinteractive.materialcalendarview.CalendarDay
+
+@BindingAdapter("event_text")
+fun TextView.eventText(event: EventModel?) {
+    if(event != null && event.eventNames.isNotEmpty()) {
+        var resultEventText = ""
+        for (eventIndex in event.eventNames.indices) {
+            resultEventText += "\n${event.eventTypes[eventIndex].toEmoji()} ${event.eventNames[eventIndex]}"
+        }
+        text = resultEventText
+    } else {
+        text = context.getString(R.string.empty_event)
+    }
+}
+
+@BindingAdapter("calendar_date_text")
+fun TextView.calendarDateText(calendarDay: CalendarDay?) {
+    if(calendarDay != null) {
+        text = "${calendarDay.month + 1}월${calendarDay.day}일"
+    }
+}
+
+@BindingAdapter("calendar_month_text")
+fun TextView.calendarMonthText(calendarDay: CalendarDay?) {
+    if(calendarDay != null) {
+        text = "${calendarDay.year}년 ${calendarDay.month + 1}월"
+    }
+}
