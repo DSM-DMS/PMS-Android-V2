0cd488c8243705a69bac1afdb23c4ebebf646965
Jaewon <turitus@naver.com>
2021-12-21 23:20:21 +0900

    [UPDATE] string dots

--- app/src/main/java/com/dms/pmsandroid/feature/calendar/CalendarViewExtension.kt
+++ app/src/main/java/com/dms/pmsandroid/feature/calendar/CalendarViewExtension.kt
@@ -1,7 +1,28 @@
 package com.dms.pmsandroid.feature.calendar
 
+import com.dms.pmsandroid.data.calendar.EventTypes
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import java.time.LocalDate
 
 fun LocalDate.toCalendarDay() =
     CalendarDay.from(year, monthValue, dayOfMonth)
+
+fun EventTypes.toEmoji(): String {
+    return when (this) {
+        EventTypes.MUST_GO_HOME -> {
+            "🟢"
+        }
+        EventTypes.EXAM -> {
+            "🟣"
+        }
+        EventTypes.VACATION -> {
+            "🟡"
+        }
+        EventTypes.HOLIDAYS -> {
+            "🔴"
+        }
+        EventTypes.ETC -> {
+            "🔵"
+        }
+    }
+}
